6c5781d5c96f69a59e196aaedcdb6edf60665923
mariaeco <maria.marcolina.eco@gmail.com>
2025-05-01 09:02:54 -0300

    removing folder

--- projeto-dispensasLicitPB/data_joincsv.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import glob 
-
-folder = glob.glob("yearfiles/*.csv")
-df_final = pd.concat([pd.read_csv(arquivo,sep=',', encoding="latin1") for arquivo in folder], ignore_index=True)
-df_final.to_csv("dislit_joined.csv",   index=False, encoding="latin1")
